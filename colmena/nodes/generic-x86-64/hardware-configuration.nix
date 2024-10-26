10159786c49711b9f4b83263743b380b4494de34
theverygaming <theverygaming@users.noreply.github.com>
2024-10-26 16:36:51 +0200

    enableRedistributableFirmware

--- colmena/nodes/generic-x86-64/hardware-configuration.nix
+++ colmena/nodes/generic-x86-64/hardware-configuration.nix
@@ -39,4 +39,6 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  hardware.enableRedistributableFirmware = true; # we do want firmware meow
 }
